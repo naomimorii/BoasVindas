bac35b633bf8887ac6828266e477cca7e88beae3
naomimorii <naomi.silva3@etec.sp.gov.br>
2020-06-14 15:35:32 -0300

    Inclusão de Projeto

---
+++ src/boasvindas/BoasVindas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boasvindas;
+
+/**
+ *
+ * @author naomi
+ */
+/*Programa que imprime 
+mensagem de Boas Vindas!*/
+
+//Definição de classe
+public class BoasVindas {
+
+    //metódo principal da classe
+    public static void main(String[] args) {
+        System.out.println("Bem Vindo ao Java!");
+    }//fim do método principal
+    
+}//fim da classe
